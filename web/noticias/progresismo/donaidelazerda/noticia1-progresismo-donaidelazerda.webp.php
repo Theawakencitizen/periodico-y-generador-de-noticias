8d16c4d235e5b20cbc26c9cbcd335293639a302d
Theawakencitizen <93624214+Theawakencitizen@users.noreply.github.com>
2023-04-26 13:16:15 -0400

    Add files via upload

---
+++ web/noticias/progresismo/donaidelazerda/noticia1-progresismo-donaidelazerda.webp.php
@@ -0,0 +1,40 @@
+<!doctype html><html lang="en">
+<head>
+<!-- metatags para buscador hemeroteca -->
+<meta name="title" content="noticia progresismo1">
+<meta name="author" content="Donai De La Zerda">
+<meta name="date" content="25 Abr, 2023">
+<meta name="lola" content="lola de la noticiax">
+<meta name="genre" content="PROGRESISMO">
+<!-- metatags para buscador hemeroteca -->
+  <!-- HTML Meta Tags -->
+  <title>noticia progresismo1</title>
+  <meta name="description" content="esta es la descripcion de la noticia">
+
+  <!-- Facebook Meta Tags -->
+  <meta property="og:url" content="https://awakencitizen.com/">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="noticia progresismo1">
+  <meta property="og:description" content="esta es la descripcion de la noticia">
+  <meta property="og:image" content="https://awakencitizen.com/noticias/imagenes-noticias/noticia1-progresismo-donaidelazerda.webp">
+
+  <!-- Twitter Meta Tags -->
+  <meta name="twitter:card" content="summary_large_image">
+  <meta property="twitter:domain" content="awakencitizen.com">
+  <meta property="twitter:url" content="https://awakencitizen.com/">
+  <meta name="twitter:title" content="noticia progresismo1">
+  <meta name="twitter:description" content="esta es la descripcion de la noticia">
+  <meta name="twitter:image" content="https://awakencitizen.com/noticias/imagenes-noticias/noticia1-progresismo-donaidelazerda.webp">
+
+  <!-- Meta Tags Generated via https://www.opengraph.xyz -->
+<meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+
+<meta charset="utf-8">
+
+<meta property="og:type" content="website">
+<meta property="og:locale" content="en_US">
+<link rel="preconnect" href="https://www.googletagmanager.com/" crossorigin="">
+
+<?php require('../../../plantilla/plantillanoticiasparte1.php')?>
+ </div> </div></div></div></div><div id="element_40" class="w-object ww_element" data-element-type="rectangle" groupid="group_6" fixed="false"><div id="element_40_content" class="content ww_rectangle"></div></div><div id="element_39" class="w-object ww_element" data-element-type="rectangle" groupid="group_6" fixed="false"><div id="element_39_content" class="content ww_rectangle"></div></div><div id="group_6" class="w-object group" data-element-type="group" data-ww_rwd="{&quot;fullWidth&quot;:{&quot;rwdMode_1&quot;:false,&quot;rwdMode_2&quot;:false,&quot;rwdMode_4&quot;:false}}" fixed="false" groupelements="[&quot;element_95&quot;,&quot;element_40&quot;,&quot;element_39&quot;]" groupid="wSection_webpage_1_default"><div id="group_6_content" class="content"></div></div><div id="element_36" class="w-object ww_element" data-element-type="text" groupid="wSection_webpage_1_default" fixed="false"><div id="element_36_content" class="content text_content"><div class="ww_text_wrapper forceLineHeight"><p style="font-size: 20px; line-height: 24px; text-align: justify;" textrwdproperties="{&quot;rwdMode_1&quot;:{&quot;fontSize&quot;:&quot;20px&quot;,&quot;lineHeight&quot;:&quot;24px&quot;,&quot;textAlign&quot;:&quot;justify&quot;,&quot;letterSpacing&quot;:&quot;&quot;},&quot;rwdMode_2&quot;:{&quot;fontSize&quot;:&quot;20px&quot;,&quot;lineHeight&quot;:&quot;24px&quot;,&quot;textAlign&quot;:&quot;justify&quot;,&quot;letterSpacing&quot;:&quot;&quot;},&quot;rwdMode_4&quot;:{&quot;fontSize&quot;:&quot;16px&quot;,&quot;lineHeight&quot;:&quot;19px&quot;,&quot;textAlign&quot;:&quot;justify&quot;,&quot;letterSpacing&quot;:&quot;&quot;}}"><span style="color:#0d0d0d;">45535453453431232131212312</span></p></div></div></div><div id="element_13" class="w-object ww_element" data-element-type="img" groupid="wSection_webpage_1_default" fixed="false"><div id="element_13_content" class="content "><div class="ww_image_wrapper"><img id="element_13_image" alt="" title="" data-ken-burns-type="disabled" class="image-uninitialized" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" width="1px" height="1px" onload="publicationApp.loadPreJsLazyImage(this);" data-ww_rwd="{&quot;data-lazy-load-src&quot;:{&quot;rwdMode_1&quot;:&quot;../../imagenes-noticias/noticia1-progresismo-donaidelazerda.webp&quot;,&quot;rwdMode_2&quot;:&quot;../../imagenes-noticias/noticia1-progresismo-donaidelazerda.webp&quot;,&quot;rwdMode_4&quot;:&quot;../../imagenes-noticias/noticia1-progresismo-donaidelazerda.webp&quot;},&quot;orginalWidth&quot;:{&quot;rwdMode_1&quot;:2048,&quot;rwdMode_2&quot;:2048,&quot;rwdMode_4&quot;:2048},&quot;wasCropped&quot;:{&quot;rwdMode_1&quot;:false,&quot;rwdMode_2&quot;:false,&quot;rwdMode_4&quot;:false},&quot;width&quot;:{&quot;rwdMode_1&quot;:786,&quot;rwdMode_2&quot;:786,&quot;rwdMode_4&quot;:323},&quot;backgroundSize&quot;:{&quot;rwdMode_1&quot;:&quot;imageCrop&quot;,&quot;rwdMode_2&quot;:&quot;imageCrop&quot;,&quot;rwdMode_4&quot;:&quot;imageCrop&quot;},&quot;data-isEnabledImageParallax&quot;:{&quot;rwdMode_1&quot;:false,&quot;rwdMode_2&quot;:false,&quot;rwdMode_4&quot;:false},&quot;backgroundPosition&quot;:{&quot;rwdMode_1&quot;:&quot;50% 50%&quot;,&quot;rwdMode_2&quot;:&quot;50% 50%&quot;,&quot;rwdMode_4&quot;:&quot;50% 50%&quot;},&quot;data-imageParallax&quot;:{&quot;rwdMode_1&quot;:80,&quot;rwdMode_2&quot;:80,&quot;rwdMode_4&quot;:80},&quot;data-used-webp-image&quot;:{&quot;rwdMode_1&quot;:true,&quot;rwdMode_2&quot;:true,&quot;rwdMode_4&quot;:true},&quot;orginalHeight&quot;:{&quot;rwdMode_1&quot;:1152,&quot;rwdMode_2&quot;:1152,&quot;rwdMode_4&quot;:1152},&quot;height&quot;:{&quot;rwdMode_1&quot;:442,&quot;rwdMode_2&quot;:442,&quot;rwdMode_4&quot;:182}}"></div></div></div><div id="element_11" class="w-object ww_element" data-element-type="simpleText" groupid="group_8" fixed="false"><div id="element_11_content" class="content "><h2 class="news_element h2_default_block">25 Abr, 2023&#xa0;</h2></div></div><div id="element_10" class="w-object ww_element" data-element-type="simpleText" groupid="group_8" fixed="false"><div id="element_10_content" class="content "><h2 class="news_element h2_default_block">Por <a href="https://awakencitizen.com/autores/Donai De La Zerda" style="color:#b33828;">Donai De La Zerda</a></h2></div></div><div id="group_8" class="w-object group" data-element-type="group" data-ww_rwd="{&quot;fullWidth&quot;:{&quot;rwdMode_1&quot;:false,&quot;rwdMode_2&quot;:false,&quot;rwdMode_4&quot;:false}}" fixed="false" groupelements="[&quot;element_11&quot;,&quot;element_10&quot;]" groupid="wSection_webpage_1_default"><div id="group_8_content" class="content"></div></div><div id="element_7" class="w-object ww_element" data-element-type="simpleText" groupid="wSection_webpage_1_default" fixed="false"><div id="element_7_content" class="content "><h2 class="news_element h2_default_block">noticia progresismo1</h2></div></div><div id="element_5" class="w-object ww_element" data-element-type="simpleText" groupid="wSection_webpage_1_default" fixed="false"><div id="element_5_content" class="content "><h2 class="news_element h2_default_block">PROGRESISMO</h2></div></div>
+<?php require('../../../plantilla/plantillanoticiasparte2.php')?>
